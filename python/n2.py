581f5d08db29d77e49b95630ab934093e511e97b
Sr Kanna <kannas@flip2.engr.oregonstate.edu>
2016-10-10 15:14:27 -0700

    added input

--- python/n2.py
+++ python/n2.py
@@ -1,10 +1,8 @@
-#!/usr/local/bin/python
-
-
-#this is a n2 program to count the number of intersections recursively for cs325
+s is a n2 program to count the number of intersections recursively for cs325
 #random function in python
 import random;
 import os;
+import sys;
 
 #print the list
 def print_list(Q):
@@ -13,11 +11,19 @@ def print_list(Q):
 
 
 #get 
-
+def input_list(P,Q,N,text):
+	fo = open(text,"r")
+	print fo.read()
+	return
 
 #define main
 def main():
+	Q = [10,15,20];
+	P = [1,2,4];
+	N = 0;
+
 	print_list(Q);		#print list
+	input_list(P,Q,N,sys.argv[1]);	
 
 
 #call main
