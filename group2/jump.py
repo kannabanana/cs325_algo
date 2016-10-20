b08348e499b3237d882c9d821ed0619ec1954443
Isaac Stallcup <stallcui@oregonstate.edu>
2016-10-20 11:17:11 -0700

    added beginning of algo from document

--- group2/jump.py
+++ group2/jump.py
@@ -5,63 +5,77 @@ import math;
 
 def read_num(idx, length):
 	fo = open(sys.argv[1], "r")
-	if idx % 2 == 0:
+	A = [[0 for x in range(2)] for y in range (0,length)]
+	if idx % 2 == 0 and idx != 6:
 		for i in range(0,idx): #get correct line
 			tmp = fo.readline().split('),')
-		print(tmp)
 		for i in range(0,len(tmp)):
 			tmp[i] = tmp[i][1:] #slice first char, open paren 
 			test = tmp[i].split(',');
-			print("test=%s" % (test))
+			A[i][0] = int(test[0])
+			A[i][1] = int(test[1].strip(')\r\n'))
+		return A
+		#FUCK YEAH INTERPRETED LANGUAGES NOT REQUING SPECIFIED RETURN TYPE!
+	elif idx == 6: #list of leash lengths
+		B = [0 for x in range(0,length)]
+		for i in range(0,idx):
+			tmp = fo.readline().split(',');
+		for i in range(0,length):
+			B[i] = int(tmp[i].strip('\r\n'))
+		return B
 		
-		return tmp
 	else: #line is singular number
 		for i in range(0,idx):
 			tmp = fo.readline();
 		return int(tmp)
 
+def J(P,i,Q,j,l):
+	#print("J with P=%s i=%s Q=%s j=%s l=%s" % (P,i,Q,j,l))
+	if i >= len(P) and j >= len(Q): #if BOTH are finished, a path exists
+		print("reached end without violating conditions")
+		return True
+	elif i >= len(P): #if one is finished but not other, then try another path
+		return False
+	elif j >= len(Q):
+		return False
+	#print("ok")	
+	#first check length, return False if length is more than l
+	dist = math.sqrt(pow((P[i][0]-Q[j][0]),2)+pow((P[i][1]-Q[j][1]),2))
+	#print("dist between P[%s] and Q[%s] = %s" % (i, j, dist))
+	#print("compare to l=%s" % (l))
+	if math.ceil(dist) > l:
+		print("dist %s > l %s" % (dist, l))
+		return False
+
+	if J(P,i+1,Q,j,l) == True:
+		return True
+	elif J(P,i,Q,j+1,l) == True:
+		return True
+	elif J(P,i+1,Q,j+1,l) == True:
+		return True
+	
+	return False #if no possible path returns true
+
 def main():
-	#huge enormous sloppy arg stuff
-#	fo = open(sys.argv[1], "r")
-#	M = fo.readline();
-#	M = int(M)
-#	print("m=%s" % (M))
-#	P = [[0 for x in range(2)] for y in range(M)]
-#	tmp = fo.readline().split('),')
-#	for i in range(0,len(tmp)):
-#		tmp[i] = tmp[i][1:]
-#		for j in range(0,len(tmp[i])):
-#			if j == 0:
-#				P[i][0] = int(tmp[i][j])
-#			if j == 2:
-#				P[i][1] = int(tmp[i][j])
-#	print(P)	
-#	N = fo.readline();
-#	N = int(N)
-#	print("n=%s" % (N))
-#	Q = [[0 for x in range(2)] for y in range(N)]
-#	tmp = fo.readline().split('),')
-#	for i in range(0,len(tmp)):
-#		tmp[i] = tmp[i][1:]
-#		for j in range(0,len(tmp[i])):
-#			if j == 0:
-#				Q[i][0] = int(tmp[i][j])
-#			if j == 2:
-#				Q[i][1] = int(tmp[i][j])
-#	print(Q)	
-#	T = fo.readline();
-#	T = int(T)
-#	print("t=%s" % (T))
-#	L = fo.readline().split(',')
-#	for i in range(0,len(L)):
-#		L[i] = int(L[i])
-#	print(L)
-#
-#	#maybe make above its own function
-#
 	M = read_num(1,0);
-	print(M);
+	print("m=%s" % (M))
+	P = [[0 for x in range(2)] for y in range(0,M)]
 	P = read_num(2,M);
-	print(P);
+	print("P=%s" % (P))
+	N = read_num(3,0);
+	print("n=%s" % (N))
+	Q = [[0 for x in range(2)] for y in range(0,N)]
+	Q = read_num(4,N);
+	print("Q=%s" % (Q))
+	T = read_num(5,0);
+	print("t=%s" % (T))
+	L = read_num(6,T);
+	L.sort()
+	print("L=%s" % (L))
+
+	for i in range(0,T):
+		print("i=%s" % (i))
+		val = J(P,0,Q,0,L[i])
+		print(val)
 
 main()
