2757a92095ce47deb959bd6ec8ea5333b8ee6d1b
Isaac Stallcup <stallcui@oregonstate.edu>
2016-10-20 22:54:03 -0700

    added some automated testing stuff in order to iron out final file I/O bug that I'm not sure is even a bug

--- group2/jump.py
+++ group2/jump.py
@@ -4,16 +4,24 @@ import re;
 import math;
 
 def read_num(idx, length):
-	fo = open(sys.argv[1], "r")
+	fo = open("input.txt", "r")
 	A = [[0 for x in range(2)] for y in range (0,length)]
+	#print("len=%s" % (length))
 	if idx % 2 == 0 and idx != 6:
 		for i in range(0,idx): #get correct line
 			tmp = fo.readline().split('),')
+#		print(tmp)
 		for i in range(0,len(tmp)):
+#			print(tmp[i])
 			tmp[i] = tmp[i][1:] #slice first char, open paren 
-			test = tmp[i].split(',');
+			test = tmp[i].split(',')
+			test[len(test)-1] = test[len(test)-1].rstrip('()\n\r')
+			#print(test)
+#			print("i=%s test=%s" % (i,test))
+#			print(len(A))
+#			print(A[i][0])
 			A[i][0] = int(test[0])
-			A[i][1] = int(test[1].strip(')\r\n'))
+			A[i][1] = int(test[1].strip(')\n'))
 		return A
 		#FUCK YEAH INTERPRETED LANGUAGES NOT REQUING SPECIFIED RETURN TYPE!
 	elif idx == 6: #list of leash lengths
@@ -21,7 +29,7 @@ def read_num(idx, length):
 		for i in range(0,idx):
 			tmp = fo.readline().split(',');
 		for i in range(0,length):
-			B[i] = int(tmp[i].strip('\r\n'))
+			B[i] = int(tmp[i].strip(')\n'))
 		return B
 		
 	else: #line is singular number
@@ -32,7 +40,7 @@ def read_num(idx, length):
 def J(P,i,Q,j,l):
 	#print("J with P=%s i=%s Q=%s j=%s l=%s" % (P,i,Q,j,l))
 	if i >= len(P) and j >= len(Q): #if BOTH are finished, a path exists
-		print("reached end without violating conditions")
+		#print("reached end without violating conditions")
 		return True
 	elif i >= len(P): #if one is finished but not other, then try another path
 		return False
@@ -44,7 +52,7 @@ def J(P,i,Q,j,l):
 	#print("dist between P[%s] and Q[%s] = %s" % (i, j, dist))
 	#print("compare to l=%s" % (l))
 	if math.ceil(dist) > l:
-		print("dist %s > l %s" % (dist, l))
+		#print("dist %s > l %s" % (dist, l))
 		return False
 
 	if J(P,i+1,Q,j,l) == True:
@@ -56,26 +64,33 @@ def J(P,i,Q,j,l):
 	
 	return False #if no possible path returns true
 
+def output(num):
+	fo = open("output.txt","w+")
+	fo.write(str(num))
+
 def main():
 	M = read_num(1,0);
-	print("m=%s" % (M))
+	#print("m=%s" % (M))
 	P = [[0 for x in range(2)] for y in range(0,M)]
 	P = read_num(2,M);
-	print("P=%s" % (P))
+	#print("P=%s" % (P))
 	N = read_num(3,0);
-	print("n=%s" % (N))
+	#print("n=%s" % (N))
 	Q = [[0 for x in range(2)] for y in range(0,N)]
 	Q = read_num(4,N);
-	print("Q=%s" % (Q))
+	#print("Q=%s" % (Q))
 	T = read_num(5,0);
-	print("t=%s" % (T))
+	#print("t=%s" % (T))
 	L = read_num(6,T);
 	L.sort()
-	print("L=%s" % (L))
+	#print("L=%s" % (L))
 
 	for i in range(0,T):
-		print("i=%s" % (i))
+		#print("i=%s" % (i))
 		val = J(P,0,Q,0,L[i])
-		print(val)
+		#print("%s=%s" % (L[i], val))
+		if val == True:
+			output(L[i])
+			break
 
 main()
