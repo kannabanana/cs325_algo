1adb7807c1d7cecf58db0fdf9c26fa694935926d
Sr Kanna <kannas@flip3.engr.oregonstate.edu>
2016-10-23 14:51:21 -0700

    dynamic solution

--- group2/dpjump.py
+++ group2/dpjump.py
@@ -10,40 +10,37 @@ import math;
 import random;
 
 
+
 #read from file
 def read_num(idx, length,text):
 	fo = open(text, "r")
 	A = [[0 for x in range(2)] for y in range (0,length)]
-	#print("len=%s" % (length))
+
+	#choose even line number, but not 6
 	if idx % 2 == 0 and idx != 6:
-		for i in range(0,idx): #get correct line
+		for i in range(0,idx): 						#get correct line
 			tmp = fo.readline().split('),')
-#		print(tmp)
 		for i in range(0,len(tmp)):
-#			print(tmp[i])
-			tmp[i] = tmp[i][1:] #slice first char, open paren 
+			tmp[i] = tmp[i][1:] 					#slice first char, open paren 
 			test = tmp[i].split(',')
 			test[len(test)-1] = test[len(test)-1].rstrip('()\n\r')
-			#print(test)
-#			print("i=%s test=%s" % (i,test))
-#			print(len(A))
-#			print(A[i][0])
 			A[i][0] = int(test[0])
 			A[i][1] = int(test[1].strip(')\n'))
 		return A
-	elif idx == 6: #list of leash lengths
+
+	elif idx == 6: 								#list of leash lengths
 		B = [0 for x in range(0,length)]
 		for i in range(0,idx):
 			tmp = fo.readline().split(',');
 		for i in range(0,length):
 			B[i] = int(tmp[i].strip(')\n'))
 		return B
-		
-	else: #line is singular number
+
+	#odd numbers
+	else: 									#line is singular number
 		for i in range(0,idx):
 			tmp = fo.readline();
 		return int(tmp)
-
 
 
 
@@ -58,13 +55,10 @@ def J(P,i,Q,j,l):
 
 	elif j >= len(Q):
 		return False
-	#print("ok")	
+
 	#first check length, return False if length is more than l
 	dist = math.sqrt(pow((P[i][0]-Q[j][0]),2)+pow((P[i][1]-Q[j][1]),2))
-	#print("dist between P[%s] and Q[%s] = %s" % (i, j, dist))
-	#print("compare to l=%s" % (l))
 	if math.ceil(dist) > l:
-		#print("dist %s > l %s" % (dist, l))
 		return False
 
 	if J(P,i+1,Q,j,l) == True:
@@ -74,8 +68,27 @@ def J(P,i,Q,j,l):
 	elif J(P,i+1,Q,j+1,l) == True:
 		return True
 	
-	return False #if no possible path returns true
+	return False 							#if no possible path returns true
 
+
+
+
+#dynamic programming solution
+def DP(P,i,Q,j):
+
+	if i >= len(P) and j >= len(Q):					 #if BOTH are finished, a path exists
+		return i
+
+	elif i >= len(P):						 #if one is finished but not other, then try another path
+		return 0
+
+	elif j >= len(Q):
+		return 0
+
+	return 1+min(DP(P,i+1,Q,j),						#min because you're trying to find the minimum leash length
+	 	 DP(P,i,Q,j+1),
+		 DP(P,i+1,Q,j+1)
+		)
 
 
 #print to text file
@@ -113,12 +126,15 @@ def main():
 #	print "L ",L,"type ",type(L), type (L[0])
 
 
-	for i in range(0,T):			#loop through number of leashes
-		#print("i=%s" % (i))
-		val = J(P,0,Q,0,L[i])
-		#print("%s=%s" % (L[i], val))
-		if val == True:
-			output(L[i])		#write to output.txt the leash length
+	val = DP(P,0,Q,0)
+
+	checker = 0
+	for x in range(0,len(L):
+		checker = L[x]
+		if val == checker:		#the list contains 
 			break
+				
+		
+	output(val)		#write to output.txt the leash length
 
 main()
