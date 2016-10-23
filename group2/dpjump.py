f7130d25396f7b0142803e41dc78ac8c5cfc2333
Sr Kanna <kannas@flip3.engr.oregonstate.edu>
2016-10-23 13:38:42 -0700

    cleaned it up

--- group2/dpjump.py
+++ group2/dpjump.py
@@ -1,9 +1,16 @@
+#NAME: SR Kanna, Isaac Stallcup
+#COURSE: Algorithms CS325
+#DATE: Fall 2016, 10/23/2016
+#INSTRUCTOR: Amir Nayyeri
+
 import os;
 import sys;
 import re;
 import math;
 import random;
 
+
+#read from file
 def read_num(idx, length,text):
 	fo = open(text, "r")
 	A = [[0 for x in range(2)] for y in range (0,length)]
@@ -24,7 +31,6 @@ def read_num(idx, length,text):
 			A[i][0] = int(test[0])
 			A[i][1] = int(test[1].strip(')\n'))
 		return A
-		#FUCK YEAH INTERPRETED LANGUAGES NOT REQUING SPECIFIED RETURN TYPE!
 	elif idx == 6: #list of leash lengths
 		B = [0 for x in range(0,length)]
 		for i in range(0,idx):
@@ -38,13 +44,18 @@ def read_num(idx, length,text):
 			tmp = fo.readline();
 		return int(tmp)
 
+
+
+
+#recursive solution
 def J(P,i,Q,j,l):
-	#print("J with P=%s i=%s Q=%s j=%s l=%s" % (P,i,Q,j,l))
-	if i >= len(P) and j >= len(Q): #if BOTH are finished, a path exists
-		#print("reached end without violating conditions")
+
+	if i >= len(P) and j >= len(Q):					 #if BOTH are finished, a path exists
 		return True
-	elif i >= len(P): #if one is finished but not other, then try another path
+
+	elif i >= len(P):						 #if one is finished but not other, then try another path
 		return False
+
 	elif j >= len(Q):
 		return False
 	#print("ok")	
@@ -66,33 +77,48 @@ def J(P,i,Q,j,l):
 	return False #if no possible path returns true
 
 
+
+#print to text file
 def output(num):
 	fo = open("output.txt","w+")
 	fo.write(str(num))
 
+
+#main
 def main():
+
+
+	#read number takes line number from input.txt, num/list and file to read from
 	M = read_num(1,0,sys.argv[1]);
-	#print("m=%s" % (M))
 	P = [[0 for x in range(2)] for y in range(0,M)]
-	P = read_num(2,M,sys.argv[1]);
-	#print("P=%s" % (P))
+	P = read_num(2,M,sys.argv[1]);				
+
+
 	N = read_num(3,0,sys.argv[1]);
-	#print("n=%s" % (N))
 	Q = [[0 for x in range(2)] for y in range(0,N)]
 	Q = read_num(4,N,sys.argv[1]);
-	#print("Q=%s" % (Q))
-	T = read_num(5,0,sys.argv[1]);
-	#print("t=%s" % (T))
-	L = read_num(6,T,sys.argv[1]);
-	L.sort()
-	#print("L=%s" % (L))
 
-	for i in range(0,T):
+	T = read_num(5,0,sys.argv[1]);
+	L = read_num(6,T,sys.argv[1]);
+	L.sort()				#sort the leases in increasing order
+
+
+
+	#types are all good!
+#	print "M ", M, "type ",type(M)
+#	print "P ",P, "type " ,type(P),P[1][0]
+#	print "N ",N,"type " ,type(N)
+#	print "Q ",Q,"type " , type(Q), type(Q[0])
+#	print "T ",T,"type ", type(T)
+#	print "L ",L,"type ",type(L), type (L[0])
+
+
+	for i in range(0,T):			#loop through number of leashes
 		#print("i=%s" % (i))
 		val = J(P,0,Q,0,L[i])
 		#print("%s=%s" % (L[i], val))
 		if val == True:
-			output(L[i])
+			output(L[i])		#write to output.txt the leash length
 			break
 
 main()
